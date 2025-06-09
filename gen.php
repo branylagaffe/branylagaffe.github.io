c935cccf58e3f9a926bcc45a5ffc522bbac90fd9
Bryan Perdrizat <bryan.perdrizat@gmail.com>
2025-06-10 00:34:43 +0200

    gen: auto generate page based on 'src.php' presence in folder

--- gen.php
+++ gen.php
@@ -1,8 +1,34 @@
 <?php
+/**
+ * Find all directory containing a 'src.php. file
+ */
+function find_articles($directory) {
+    $folder_with_src = [];
 
+    // Use RecursiveDirectoryIterator to traverse the directory
+    $iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($directory));
+
+    foreach ($iterator as $fileInfo) {
+        // Check if the current item is a file and its name is 'src.php'
+        if ($fileInfo->isFile() && $fileInfo->getFilename() === 'src.php') {
+            // Get the directory of the file and add it to the array if not already present
+            $folder_path = $fileInfo->getPath();
+            if (!in_array($folder_path, $folder_with_src)) {
+                $folder_with_src[] = $folder_path;
+            }
+        }
+    }
+
+    return $folder_with_src;
+}
+
+/**
+ * Generated on page based on the diretory
+ *
+ */
 function generate($src_dir) {
-    $dst = $src_dir . "/index.html";
-    $src = $src_dir . "/src.php";
+    $dst = basename($src_dir) . "/index.html";
+    $src = basename($src_dir) . "/src.php";
 
     $cwd = getcwd();
 
@@ -19,15 +45,18 @@ function generate($src_dir) {
     chdir($cwd);
 
     ob_end_clean();
-
     file_put_contents($dst, $contents);
 
     echo("Generated $dst \n");
 }
 
+/**
+* MAIN SCRIPT
+*/
+$folders = find_articles(".");
+foreach ($folders as $key => $folder) {
+    generate($folder);
+}
 
-// List of generation (manual)
-generate(".");
-generate("test");
 ?>
 
