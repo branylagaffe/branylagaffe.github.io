8508699bfc4a15b7e1daa085989d88bb06f4220d
Bryan Perdrizat <bryan.perdrizat@gmail.com>
2025-06-13 12:09:05 +0200

    article: disable test articles

--- src.php
+++ src.php
@@ -4,7 +4,7 @@
     </section>
     <article>
         <ul class="article__list">
-            <li><a href="test/index.html">What the fuck am I doing ?</a><time datetime="1996-01-17">17/01/1996</time></li>
+            <!-- <li><a href="test/index.html">What the fuck am I doing ?</a><time datetime="1996-01-17">17/01/1996</time></li> -->
         </ul>
     </article>
 </main>
