711360ff3f5d8dd56ee4426f313e56849bb3046f
Bryan Perdrizat <bryan.perdrizat@gmail.com>
2025-06-13 12:07:52 +0200

    about: create page

---
+++ about/src.php
@@ -0,0 +1,10 @@
+<main>
+    <section>
+        <h1>About</h1>
+    </section>
+    <article>
+    <p>You landed on my personal website. Many words and thoughts failed to make their way to this website, I like it nonetheless. It belongs to me as much it belongs to you.</p>
+
+    <p>An insane amount of <em>Disco Elysium</em> quotes and nonsense are, however, very present.</p>
+    </article>
+</main>
